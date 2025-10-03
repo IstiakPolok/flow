762f91828eb5e8420f3ebe809659e22b61e984de
Fatin Istiak Polok <fatin15-1944@diu.edu.bd>
2025-10-03 14:30:59 +0600

    Update auto_commit.sh

--- automated-commits/auto_commit.sh
+++ automated-commits/auto_commit.sh
@@ -2,7 +2,7 @@
  
 # Navigate to the local repository
 cd /c/Users/Polok/flow
-
+ 
 # Create or update a file (you can name the file anything you like)
 echo "Automated commit on $(date)" > file.txt
 
